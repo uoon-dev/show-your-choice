4b6d1919c306420d1c4da60680c2594a7c0206ed
uoon-dev <uoon.dev@gmail.com>
2018-09-06 18:59:05 +0900

    make datas

---
+++ src/views/Question.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="question">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+<style lang="scss" scoped>
+
+</style>
+<script>
+  import questions from '../data/questions';
+  export default {
+    data() {
+      return {
+        title: ''
+      }
+    },
+    created() {
+      debugger;
+      this.title = questions[0].title;
+      // this.axios.get('./src/data/questions.js').then((response) => {
+      //   console.log(response.data)
+      // })
+    }
+  }
+</script>
