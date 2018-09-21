e546a9b314e71aaab001fd4a52e3829114af6e65
uoon-dev <uoon.dev@gmail.com>
2018-09-21 15:01:25 +0900

    change input style on add question component

--- src/components/AddQuestionItems.vue
+++ src/components/AddQuestionItems.vue
@@ -4,52 +4,49 @@
       <p data-title-questions>
         <el-row :gutter="20">
           <el-col :span="16" :offset="5">
-            <el-form-item data-title>
-              <h2>{{ header }}</h2>
-              <el-input 
-                type="text" 
+              <input
+                :class="[question.title.length > 0 ? 'stay' : '', 'paori']"
                 v-model="question.title" 
-                @keyup.native="updateQuestionTitle"
-                clearable 
+                @keyup="updateQuestionTitle"
                 autofocus>
-              </el-input>
-            </el-form-item>
+              </input>
+              <label data-title>질문</label>
+              <div data-border-bottom></div>
           </el-col>
         </el-row>
       </p>
       <p data-options>
         <el-row :gutter="20">
           <el-col :span="12" :offset="7">
-            <el-form-item data-title>
-              <h3>+ 옵션</h3>
-                <el-input
-                  type="text" 
-                  v-for="(option, index) in optionData.options" 
-                  v-model="option.text"
-                  @keydown.enter.native="enterAndGo"
-                  @keyup.native="updateOptionContent(option, index)"
-                  :class="{rightPadding: index !== 0}"
-                  :key="index"
-                  :data-index="index"
-                  style="margin-bottom: 10px;">
-                  <i
-                    class="el-icon-error el-input__icon"
-                    slot="suffix"
-                    style="cursor: pointer;"
-                    @click="clearText($event, option)">
-                  </i>
-                  <i
-                    class="el-icon-delete el-input__icon"
-                    slot="suffix"
-                    style="cursor: pointer;"
-                    v-if="index === 0 ? false : true"
-                    @click="deleteOption(option)">
-                  </i>
-                  <template slot="append" v-if="showAddButton(option)">
-                    <el-button type="primary" icon="el-icon-circle-plus" circle @click="enterAndGo"></el-button>
-                  </template>
-                </el-input>                
-            </el-form-item>
+            <div data-options-content v-for="(option, index) in optionData.options" :key="index">
+              <input
+                v-model="option.text"
+                @keydown.enter="enterAndGo"
+                @keyup="updateOptionContent(option, index)"
+                :class="[{rightPadding: index !== 0}, [option.text.length > 0 ? 'stay' : '', 'ggobugi']]"
+                
+                :data-index="index"
+                style="margin-bottom: 10px;">
+                <!-- <i
+                  class="el-icon-error el-input__icon"
+                  slot="suffix"
+                  style="cursor: pointer;"
+                  @click="clearText($event, option)">
+                </i>
+                <i
+                  class="el-icon-delete el-input__icon"
+                  slot="suffix"
+                  style="cursor: pointer;"
+                  v-if="index === 0 ? false : true"
+                  @click="deleteOption(option)">
+                </i>
+                <template slot="append" v-if="showAddButton(option)">
+                  <el-button type="primary" icon="el-icon-circle-plus" circle @click="enterAndGo"></el-button>
+                </template> -->
+              </input>
+              <label data-title>옵션</label>
+              <div data-options-box></div>
+            </div>
           </el-col>
         </el-row>              
       </p>
@@ -60,15 +57,151 @@
   </span>
 </template>
 <style lang="scss" scoped>
+  [data-title-questions] {
+    position: relative;
+    margin-bottom: 100px;
+    
+    input {
+      width: 90%;
+      height: 35px;
+      padding: 0 10px 7px 10px;
+      font-size: 24px;
+      border: none;
+      border-bottom: 2px solid gainsboro;
+      outline: none;
+
+      &.stay ~ [data-title] {
+          top: -25px;
+          left: 25%;
+          font-size: 15px;
+          color: steelblue;
+      }
+
+      &.stay ~ [data-border-bottom] {
+         width: 60.5%;     
+      }
+
+      &.paori {
+        &:focus ~ [data-title] {
+          top: -25px;
+          left: 25%;
+          font-size: 15px;
+          color: steelblue;
+        }
+          
+        &:focus ~ [data-border-bottom] {
+          width: 60.5%;
+        }
+      }
+    }
+
+    [data-title] {
+      position: absolute;
+      top: 15%;
+      left: 25%;
+      font-weight: 600;
+      color: gray;
+      transition: all 0.3s cubic-bezier(0.18, 0.87, 0.74, 1.51);
+    }
+  
+    [data-border-bottom] {
+      position: absolute;
+      bottom: 0.5px;
+      left: 24%;
+      width: 0;
+      height: 2px;
+      background: steelblue;
+      transition: all 0.2s ease-out;
+    }
+  }
+
+  [data-options] {
+    [data-options-content] {
+      position: relative;
+      margin-bottom: 60px;
+
+      input {
+        width: 80%;
+        height: 40px;
+        margin-top: -20px;
+        padding: 0 10px 7px 10px;
+        font-size: 18px;
+        border: none;
+        border-bottom: 2px solid gainsboro;
+        outline: none;
+        transition: all 0.3s ease-out;
+
+        &.stay {
+          height: 30px;
+          margin-top: 0px;
+          padding: 3px 10px 3px 10px;
+          
+          & ~ [data-title] {
+            top: -24px;
+            left: 8%;
+            font-size: 12px;
+            color: white;
+          }
+
+          & ~ [data-options-box] {
+            position: absolute;
+            top: -31px;
+            left: 2%;
+            z-index: -1;
+            width: 95%;
+            background: steelblue;
+            height: 78px;   
+          }
+        }
+
+        &.ggobugi {
+          &:focus ~ [data-title] {
+            top: -24px;
+            left: 8%;
+            font-size: 12px;
+            color: white;
+          }
+
+          &:focus ~ [data-options-box] {
+            position: absolute;
+            top: -31px;
+            left: 2%;
+            z-index: -1;
+            width: 95%;
+            background: steelblue;
+            height: 78px;   
+          }
+
+          &:focus {
+            height: 30px;
+            margin-top: 0px;
+            padding: 3px 10px 3px 10px;
+          }
+        }
+      }
+
+      [data-title] {
+        position: absolute;
+        top: 2px;
+        left: 10%;
+        font-size: 15px;
+        font-weight: 600;
+        color: gray;
+        transition: all 0.3s ease-out; 
+      }
+      
+      [data-options-box] {
+        transition: all 0.3s ease-out;
+      }
+    }
+  }
+   
+
   [data-versus] {
     position: absolute;
     top: 20vh;
     left: 50%;
     transform: translateX(-50%)
-  }
-
-  .border-gray {
-    border: 1px solid gainsboro;
   }
 
   .rightPadding {
@@ -95,7 +228,6 @@ export default {
         title: '',
         options: [],
       },
-      header: this.header,
       optionContent: {},
       optionData: ''
     }
@@ -117,23 +249,20 @@ export default {
   },
   methods : {
     enterAndGo(e) {
-      let input;
-      e.currentTarget.nodeName === 'BUTTON' ? 
-        input = e.currentTarget.parentElement.parentElement :
-        input = e.currentTarget;
-
-      const inputIndex = input.children[0].dataset.index;
+      const input = e.currentTarget;
+      const inputIndex = input.dataset.index;
+      let nextElement = input.parentElement.nextElementSibling;
       
-      if (input.nextSibling.nodeName === 'DIV') {
-        const nextInput = input.nextSibling.children[0]; 
+      if (nextElement) {
+        const nextInput = nextElement.children[0]; 
         nextInput.focus();
       } else {
         this.optionData.options.push({text:''});
 
         // after element is created...
         setTimeout(function() {
-          const nextInput = input.nextSibling.children[0];
-          nextInput.style.paddingRight = '60px';
+          nextElement = input.parentElement.nextElementSibling;
+          const nextInput = nextElement.children[0]; 
           nextInput.focus();
         }.bind(this), 100)
       }
